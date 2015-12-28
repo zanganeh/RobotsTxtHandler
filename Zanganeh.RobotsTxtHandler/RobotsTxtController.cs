a274e6e940d573f724e924ff5020c9df300ef3fe
Aria Zanganeh <ariazanganeh@levo.com.au>
2015-12-29 09:14:18 +1100

    First initialization

---
+++ Zanganeh.RobotsTxtHandler/RobotsTxtController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Zanganeh.RobotsTxtHandler
+{
+	public class RobotsTxtController : Controller
+	{
+		public ActionResult Index()
+		{
+			return Content("User-agent: *");
+		}
+	}
+}
---
+++ Zanganeh.RobotsTxtHandler/RobotsTxtHandlerInitialization.cs
@@ -0,0 +1,22 @@
+﻿using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.ServiceLocation;
+using EPiServer.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Zanganeh.RobotsTxtHandler
+{
+	[ModuleDependency(typeof(EPiServer.Web.InitializationModule))]
+	public class RobotsTxtHandlerInitialization : IInitializableModule
+	{
+		public void Initialize(InitializationEngine context)
+		{
+			RouteTable.Routes.MapRoute("RobotsTxtHandlerRoute", "robots.txt", new { controller = "RobotsTxt", action = "Index" });
+		}
+
+		public void Uninitialize(InitializationEngine context)
+		{
+		}
+	}
+}
