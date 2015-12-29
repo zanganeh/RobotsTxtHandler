24608f8442a29292e9f67b987a01f6be4f65822f
Aria Zanganeh <ariazanganeh@levo.com.au>
2015-12-29 15:58:03 +1100

    Add support NuGet package

--- Zanganeh.RobotsTxtHandler.NuGet/Properties/AssemblyInfo.cs
+++ Zanganeh.RobotsTxtHandler.NuGet/Properties/AssemblyInfo.cs
@@ -1,14 +1,15 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Zanganeh.RobotsTxtHandler")]
-[assembly: AssemblyDescription("rotobs.txt implementation for EPiServer")]
+[assembly: AssemblyTitle("Zanganeh.RobotsTxtHandler.NuGet")]
+[assembly: AssemblyDescription("Project template made by EyeCatch (http://www.eyecatch.no/)")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Zanganeh")]
-[assembly: AssemblyProduct("Zanganeh.RobotsTxtHandler")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Zanganeh.RobotsTxtHandler.NuGet")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("754a389b-6f05-4c49-96d1-d8d34e9ef7bf")]
+[assembly: Guid("5b586874-8d3e-4137-9ba8-e982413416d5")]
 
 // Version information for an assembly consists of the following four values:
 //
