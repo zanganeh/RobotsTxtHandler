2afcb258a9477e4ee2fe0f2168c48e071b1e2dd8
Aria Zanganeh <ariazanganeh@levo.com.au>
2016-11-08 12:36:49 +1100

    Add  build.bat

---
+++ build.bat
@@ -0,0 +1,17 @@
+@echo Off
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+
+set version=
+if not "%PackageVersion%" == "" (
+   set version=-Version %PackageVersion%
+)
+
+REM Build
+"%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" Zanganeh.RobotsTxtHandler.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+
+REM Package
+mkdir Build
+call ".nuget\nuget.exe" pack "Zanganeh.RobotsTxtHandler\Zanganeh.RobotsTxtHandler.csproj" -IncludeReferencedProjects -o Build -p Configuration=%config% %version%
