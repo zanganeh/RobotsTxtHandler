be671957b400f56315c321556179fafa1f163839
Aria Zanganeh <ariazanganeh@levo.com.au>
2016-11-08 12:50:14 +1100

    Change to restore

--- build.bat
+++ build.bat
@@ -9,6 +9,9 @@ if not "%PackageVersion%" == "" (
    set version=-Version %PackageVersion%
 )
 
+REM Restore NuGet
+call ".nuget\nuget.exe" restore Zanganeh.RobotsTxtHandler.sln
+
 REM Build
 "%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" Zanganeh.RobotsTxtHandler.sln /p:Configuration="%config%" /p:Platform="Any CPU" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
